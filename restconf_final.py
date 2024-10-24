73b356b5ae89061f3d7a87f0bb5926281a534319
KIT4ER0 <tae.tanakit47@gmail.com>
2024-10-24 19:27:28 +0700

    update create

--- restconf_final.py
+++ restconf_final.py
@@ -3,7 +3,7 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 # Router IP Address is 10.0.15.189
-api_url = "https://10.0.15.130/restconf/data/ietf-interfaces:interfaces/interface=Loopback65070090"
+api_url = "https://10.0.15.189/restconf/data/ietf-interfaces:interfaces/interface=Loopback65070090"
 
 # the RESTCONF HTTP headers, including the Accept and Content-Type
 # Two YANG data formats (JSON and XML) work with RESTCONF 
@@ -14,95 +14,111 @@ basicauth = ("admin", "cisco")
 
 
 def create():
-    yangConfig = <!!!REPLACEME with YANG data!!!> 
+    yangConfig = {
+        "ietf-interfaces:interface": {
+        "name": "Loopback65070090",
+        "type": "iana-if-type:softwareLoopback",
+        "enabled": True,
+        "ietf-ip:ipv4": {
+            "address": [
+                {
+                    "ip": "172.30.90.1",
+                    "netmask": "255.255.255.0"
+                }
+            ]
+        },
+        "ietf-ip:ipv6": {}
+    }
+}
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+    resp = requests.put(
+        api_url, 
+        data=json.dumps(yangConfig), 
         auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
+        headers=headers, 
         verify=False
         )
-
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
+    if(resp.status_code == 204):
+        print("Cannot create: Interface loopback 66070090")
+    elif(resp.status_code >= 200 and resp.status_code <= 299):
+        print(resp.status_code)
+        print("Interface loopback 66070090 is created successfully")
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
 
 
-def delete():
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+# def delete():
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         auth=basicauth, 
+#         headers=<!!!REPLACEME with HTTP Header!!!>, 
+#         verify=False
+#         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
-
-
-def enable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
-
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
-
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
 
 
-def disable():
-    yangConfig = <!!!REPLACEME with YANG data!!!>
+# def enable():
+#     yangConfig = <!!!REPLACEME with YANG data!!!>
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+#         auth=basicauth, 
+#         headers=<!!!REPLACEME with HTTP Header!!!>, 
+#         verify=False
+#         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
 
 
-def status():
-    api_url_status = "<!!!REPLACEME with URL of RESTCONF Operational API!!!>"
+# def disable():
+#     yangConfig = <!!!REPLACEME with YANG data!!!>
 
-    resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-        <!!!REPLACEME with URL!!!>, 
-        auth=basicauth, 
-        headers=<!!!REPLACEME with HTTP Header!!!>, 
-        verify=False
-        )
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
+#         auth=basicauth, 
+#         headers=<!!!REPLACEME with HTTP Header!!!>, 
+#         verify=False
+#         )
 
-    if(resp.status_code >= 200 and resp.status_code <= 299):
-        print("STATUS OK: {}".format(resp.status_code))
-        response_json = resp.json()
-        admin_status = <!!!REPLACEME!!!>
-        oper_status = <!!!REPLACEME!!!>
-        if admin_status == 'up' and oper_status == 'up':
-            return "<!!!REPLACEME with proper message!!!>"
-        elif admin_status == 'down' and oper_status == 'down':
-            return "<!!!REPLACEME with proper message!!!>"
-    elif(resp.status_code == 404):
-        print("STATUS NOT FOUND: {}".format(resp.status_code))
-        return "<!!!REPLACEME with proper message!!!>"
-    else:
-        print('Error. Status Code: {}'.format(resp.status_code))
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
+
+
+# def status():
+#     api_url_status = "<!!!REPLACEME with URL of RESTCONF Operational API!!!>"
+
+#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
+#         <!!!REPLACEME with URL!!!>, 
+#         auth=basicauth, 
+#         headers=<!!!REPLACEME with HTTP Header!!!>, 
+#         verify=False
+#         )
+
+#     if(resp.status_code >= 200 and resp.status_code <= 299):
+#         print("STATUS OK: {}".format(resp.status_code))
+#         response_json = resp.json()
+#         admin_status = <!!!REPLACEME!!!>
+#         oper_status = <!!!REPLACEME!!!>
+#         if admin_status == 'up' and oper_status == 'up':
+#             return "<!!!REPLACEME with proper message!!!>"
+#         elif admin_status == 'down' and oper_status == 'down':
+#             return "<!!!REPLACEME with proper message!!!>"
+#     elif(resp.status_code == 404):
+#         print("STATUS NOT FOUND: {}".format(resp.status_code))
+#         return "<!!!REPLACEME with proper message!!!>"
+#     else:
+#         print('Error. Status Code: {}'.format(resp.status_code))
