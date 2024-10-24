aa38189c155bc23ec882f0f9f995463c14be81bd
KIT4ER0 <tae.tanakit47@gmail.com>
2024-10-24 19:44:25 +0700

    update delete

--- restconf_final.py
+++ restconf_final.py
@@ -39,27 +39,31 @@ def create():
         verify=False
         )
     if(resp.status_code == 204):
-        print("Cannot create: Interface loopback 66070090")
+        print(resp.status_code)
+        return "Cannot create: Interface loopback 65070090"
     elif(resp.status_code >= 200 and resp.status_code <= 299):
         print(resp.status_code)
-        print("Interface loopback 66070090 is created successfully")
+        return "Interface loopback 65070090 is created successfully"
     else:
         print('Error. Status Code: {}'.format(resp.status_code))
 
 
-# def delete():
-#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-#         <!!!REPLACEME with URL!!!>, 
-#         auth=basicauth, 
-#         headers=<!!!REPLACEME with HTTP Header!!!>, 
-#         verify=False
-#         )
+def delete():
+    resp = requests.delete(
+        api_url, 
+        auth=basicauth, 
+        headers=headers, 
+        verify=False
+        )
 
-#     if(resp.status_code >= 200 and resp.status_code <= 299):
-#         print("STATUS OK: {}".format(resp.status_code))
-#         return "<!!!REPLACEME with proper message!!!>"
-#     else:
-#         print('Error. Status Code: {}'.format(resp.status_code))
+    if(resp.status_code >= 200 and resp.status_code <= 299):
+        print("STATUS OK: {}".format(resp.status_code))
+        return "Interface loopback 65070090 is deleted successfully"
+    elif(resp.status_code == 404):
+        print(resp.status_code)
+        return "Cannot delete: Interface loopback 65070090"
+    else:
+        print('Error. Status Code: {}'.format(resp.status_code))
 
 
 # def enable():
