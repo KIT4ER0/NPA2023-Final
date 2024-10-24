561edc80ef445249e1c4772bc2ec61d068eb91c2
KIT4ER0 <tae.tanakit47@gmail.com>
2024-10-24 20:03:35 +0700

    update disable

--- restconf_final.py
+++ restconf_final.py
@@ -93,22 +93,31 @@ def enable():
         print('Error. Status Code: {}'.format(resp.status_code))
 
 
-# def disable():
-#     yangConfig = <!!!REPLACEME with YANG data!!!>
+def disable():
+    yangConfig = {
+            "ietf-interfaces:interface": {
+                "name": "Loopback65070090",
+                "type": "iana-if-type:softwareLoopback",
+                "enabled": False
+            }
+        }
 
-#     resp = requests.<!!!REPLACEME with the proper HTTP Method!!!>(
-#         <!!!REPLACEME with URL!!!>, 
-#         data=json.dumps(<!!!REPLACEME with yangConfig!!!>), 
-#         auth=basicauth, 
-#         headers=<!!!REPLACEME with HTTP Header!!!>, 
-#         verify=False
-#         )
+    resp = requests.patch(
+        api_url, 
+        data=json.dumps(yangConfig), 
+        auth=basicauth, 
+        headers=headers, 
+        verify=False
+        )
 
-#     if(resp.status_code >= 200 and resp.status_code <= 299):
-#         print("STATUS OK: {}".format(resp.status_code))
-#         return "<!!!REPLACEME with proper message!!!>"
-#     else:
-#         print('Error. Status Code: {}'.format(resp.status_code))
+    if(resp.status_code >= 200 and resp.status_code <= 299):
+        print("STATUS OK: {}".format(resp.status_code))
+        return "Interface loopback 65070090 is shutdowned successfully"
+    elif(resp.status_code == 404):
+        print(resp.status_code)
+        return "Cannot shutdown: Interface loopback 65070090"
+    else:
+        print('Error. Status Code: {}'.format(resp.status_code))
 
 
 # def status():
